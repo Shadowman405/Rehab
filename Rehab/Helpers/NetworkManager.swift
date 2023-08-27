23f0c27e1b327334a0ca2ae9429ebe47719b9277
Shadowman405 <maximus405@gmail.com>
2023-08-27 21:15:00 +0300

    write logic to NetManager

--- Rehab/Helpers/NetworkManager.swift
+++ Rehab/Helpers/NetworkManager.swift
@@ -10,7 +10,19 @@ import Foundation
 class NetworkManager {
     static let shared = NetworkManager()
     
-    func getPeople(){
-        
+    func getPeople(completion: @escaping (Result<[Person], Error>) -> Void) {
+        guard let url = URL(string: "https://swapi.dev/api/people")
+        else {return}
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let persons = try JSONDecoder().decode([Person].self, from: data)
+                    completion(.success(persons))
+                }
+                catch {
+                    completion(.failure(error.localizedDescription as! Error))
+                }
+            }
+        }
     }
 }
