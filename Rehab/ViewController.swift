e67449930991873b1a947a2889f672e9d75e5f7c
Shadowman405 <maximus405@gmail.com>
2023-08-27 20:37:21 +0300

    Initial Commit

---
+++ Rehab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rehab
+//
+//  Created by Maxim Mitin on 27.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
