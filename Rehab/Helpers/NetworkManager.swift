e852028400be34bb9242be0a9b6c076cc6798be5
Shadowman405 <maximus405@gmail.com>
2023-08-27 21:29:13 +0300

    minor updates

--- Rehab/Helpers/NetworkManager.swift
+++ Rehab/Helpers/NetworkManager.swift
@@ -10,19 +10,20 @@ import Foundation
 class NetworkManager {
     static let shared = NetworkManager()
     
-    func getPeople(completion: @escaping (Result<[Person], Error>) -> Void) {
+    func getPeople(completion: @escaping (Result<SwapiPersonResults, Error>) -> Void) {
         guard let url = URL(string: "https://swapi.dev/api/people")
         else {return}
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
                 do {
-                    let persons = try JSONDecoder().decode([Person].self, from: data)
+                    let persons = try JSONDecoder().decode(SwapiPersonResults.self, from: data)
+                    print(persons.results)
                     completion(.success(persons))
                 }
                 catch {
                     completion(.failure(error.localizedDescription as! Error))
                 }
             }
-        }
+        }.resume()
     }
 }
--- Rehab/ViewController.swift
+++ Rehab/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var peoples: [Person] = []
+    let networkManager = NetworkManager.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        NetworkManager.shared.getPeople { response in
+            switch response{
+            case .success(let peoples):
+                print(peoples)
+                self.peoples = peoples.results
+            case.failure(let error):
+                print(error)
+            }
+        }
     }
 
+    @IBAction func btnCLicked(_ sender: Any) {
+        
+        for i in peoples {
+            print(i)
+        }
+    }
     
 
 }
