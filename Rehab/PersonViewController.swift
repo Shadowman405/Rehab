5c6f2733d8c390e3b404e427bbe3202a81b5be5c
Shadowman405 <maximus405@gmail.com>
2023-08-28 22:47:12 +0300

    add new VC

---
+++ Rehab/PersonViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PersonViewController.swift
+//  Rehab
+//
+//  Created by Maxim Mitin on 28.08.23.
+//
+
+import UIKit
+
+class PersonViewController: UIViewController {
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var massLbl: UILabel!
+    @IBOutlet weak var genderLbl: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
